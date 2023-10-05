d8fd62ac898cb52389cdb83139fdc40d80f31fd1
vlobzakov <39057925+vlobzakov@users.noreply.github.com>
2023-10-05 14:38:27 +0300

    Create creds-replace.js

---
+++ scripts/creds-replace.js
@@ -0,0 +1,8 @@
+    //adminAuth: {
+    //    type: "credentials",
+    //    users: [{
+    //        username: "admin",
+    //        password: "$2a$08$zZWtXTja0fB1pzD4sHCMyOCMYz2Z6dNbM6tl8sJogENOMcxWV9DN.",
+    //        permissions: "*"
+    //    }]
+    //},

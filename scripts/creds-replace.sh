4b597d484ee3c44b9e77e2dc99c16f14c42e2e03
vlobzakov <39057925+vlobzakov@users.noreply.github.com>
2023-10-05 15:06:57 +0300

    Update creds-replace.sh

--- scripts/creds-replace.sh
+++ scripts/creds-replace.sh
@@ -1,4 +1,4 @@
 lead='^.*\/\/adminAuth: {'
 tail='^.*\/\*\* The following property can be used to enable HTTPS'
-sed -e "/$lead/,/$tail/{ /$lead/{p; r creds-replace.js
-}; /$tail/p; d }" -i settings.js
+sed -e "/$lead/,/$tail/{ /$lead/{p; r /root/creds-replace.js
+}; /$tail/p; d }" -i /data/settings.js

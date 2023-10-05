af98f4451387041d3bee6b30b6b6c81d61df5ef3
vlobzakov <39057925+vlobzakov@users.noreply.github.com>
2023-10-05 14:51:09 +0300

    Update creds-replace.js

--- scripts/creds-replace.js
+++ scripts/creds-replace.js
@@ -1,8 +1,8 @@
-    //adminAuth: {
-    //    type: "credentials",
-    //    users: [{
-    //        username: "admin",
-    //        password: "$2a$08$zZWtXTja0fB1pzD4sHCMyOCMYz2Z6dNbM6tl8sJogENOMcxWV9DN.",
-    //        permissions: "*"
-    //    }]
-    //},
+    adminAuth: {
+        type: "credentials",
+        users: [{
+            username: "admin",
+            password: "*********WtXTja0fB1pzD4sHCMyOCMYz2Z6dNbM6tl8sJogENOMcxWV9DN.",
+            permissions: "*"
+        }]
+    },

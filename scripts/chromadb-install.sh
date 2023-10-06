6c24126c6d3fcf839052a30b5896461934a22b42
vlobzakov <39057925+vlobzakov@users.noreply.github.com>
2023-10-06 09:23:00 +0300

    Update chromadb-install.sh

--- scripts/chromadb-install.sh
+++ scripts/chromadb-install.sh
@@ -2,5 +2,5 @@ git clone https://github.com/chroma-core/chroma
 cd chroma/
 docker-compose up --build -d
 sleep 10
-docker_id=$(docker ps | grep chroma:latest | awk -F\  '{print $1}')
+docker_id=$(docker ps | grep chroma-server | awk -F\  '{print $1}')
 docker update --restart=always $docker_id
